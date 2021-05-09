c9057182e0f3f948dc8b416af3b8df01e9fd4d11
Levent Batakci <61567072+Levent-Batakci@users.noreply.github.com>
2021-05-09 16:38:45 -0400

    Delete Functions/Preprocessing directory

--- Functions/Preprocessing/DiscreteGrayScale.m
+++
@@ -1,7 +0,0 @@
-function images = DiscreteGrayScale(discretizedImages)
-%Recovers images from discrete version
-
-spacing = 255/k;
-images = floor( (floor(discretizedImages * spacing)+ceil(discretizedImages*spacing)) /2 );
-end
-
